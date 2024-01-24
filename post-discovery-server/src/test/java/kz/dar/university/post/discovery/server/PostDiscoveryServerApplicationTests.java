e62ec9d804128c6847a558af19f47a3293126f57
Eldar <zhakenov.eldar@gmail.com>
2024-01-25 01:23:18 +0600

    eureka serever

---
+++ post-discovery-server/src/test/java/kz/dar/university/post/discovery/server/PostDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.university.post.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
